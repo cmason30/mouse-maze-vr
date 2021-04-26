8329548689de4240c42a026e7c849a46a07c8783
cmason30 <colinpetermason@gmail.com>
2021-04-26 11:38:43 -0600

    Cleaned up functions.

--- colin_funcs.py
+++ colin_funcs.py
@@ -1,87 +1,75 @@
 import os
 import pandas as pd
+import colin_sub_funcs
 from shapely.geometry import Point
-from shapely.geometry import Polygon, LineString, LinearRing
 import numpy as np
 
-# Gets fed the master sheet and maze coordinates.
-# dist_threshold has to be between 0 and 1
+'''
+mouse_edge_distance()
+Detects when mouse is near boundaries of maze.
 
-def mouse_edge_distance(df_input, maze_coordinates, distance_threshold=.9):
-    df_test = df_input.copy()
+Input
+behavioral_df: Behavioral data as panddas dataframe
+maze_coordinates: String label of maze. Options include: [square, OF, corridor, ymaze, circle]
+distance_threshold: The percent distance from wall of mouse. Must be between 0 and 1
 
-    # Checks if coordinates are a circle radius.
-    if isinstance(maze_coordinates, int):
-        p = Point(0, 0)
-        inner_circle = maze_coordinates * distance_threshold
-        polygon_maze = p.buffer(maze_coordinates)
-        red_poly_main = polygon_maze.difference(Point(0.0, 0.0).buffer(inner_circle)) # <- donut
+Output
+Dataframe of two columns:
+    mouse_dist - unit distance from wall for each coordinate
+    distance_marked - marks coordinates with 1 if mouse is within distance threshold that was set
+'''
 
-    else:
-        ident_mat = np.zeros((2, 2), float)
-        np.fill_diagonal(ident_mat, distance_threshold)
-        coords_t = maze_coordinates.transpose()
-        coords_red = np.matmul(ident_mat, coords_t)
-        polygon_maze = Polygon(maze_coordinates)
-        red_lin = LinearRing(coords_red.transpose())
-        red_poly_main = Polygon(maze_coordinates, [red_lin])
+def mouse_edge_distance(behavioral_df, maze_coordinates, distance_threshold=.1):
+    df_test = behavioral_df.copy()
+
+    # Gets inner shape to make distance threshold boundary polygons
+    polygon_maze, reduced_poly = colin_sub_funcs.shape_shrink(maze_coordinates, distance_threshold)
 
     # Checks coordinates in each row and returns the mouses distance from the edge and True or False if mouse is
     # within the threshold distance to the wall
     df_test["mouse_dist"] = df_test.apply(lambda row: polygon_maze.exterior.distance(Point(row["Position.X"], row["Position.Y"])), axis=1)
-    df_test['distance_marked'] = df_test.apply(lambda row: 1 if red_poly_main.contains(Point(row["Position.X"], row["Position.Y"])) else 0, axis=1)
+    df_test['distance_marked'] = df_test.apply(lambda row: 1 if reduced_poly.contains(Point(row["Position.X"], row["Position.Y"])) else 0, axis=1)
 
     # Checks that all coordinates are in Polygon
-    df_test['in_poly'] = df_test.apply(lambda row: polygon_maze.contains(Point(row["Position.X"], row["Position.Y"])), axis=1)
-    if (~df_test['in_poly']).sum() != 0:
-        raise Exception('Warning! Mouse movement detected outside of Polygon boundaries.')
+    colin_sub_funcs.within_bounds(df_test, polygon_maze)
 
     # outputs two columns: mouse_dist, distance_marked
     return df_test[['mouse_dist', 'distance_marked']]
 
-# Function created for use in the y_maze_time_spent() function
-
-def region_match(row):
-    if row['top']:
-        return 'top'
-    elif row['center']:
-        return 'center'
-    elif row['right']:
-        return 'right'
-    elif row['left']:
-        return 'left'
-    else:
-        raise Exception('Warning! Mouse movement detected outside of Polygon boundaries.')
 
 
-# Input is the behavioral data.
+'''
+y_maze_time_spent()
+Gets a dictionary of time spent in each region along with [[time_diff, region]] master sheet columns
+
+Input
+
+behavioral_df: the mouse behavioral dataframe
+
+Output
+
+time_spent dictionary, columns of time_diff and region
+'''
 
 def y_maze_time_spent(behavioral_df):
-    # Separates each region as different polygons
-    top = Polygon(LineString([(-125, 513.924), (-125, 13.925), (125, 13.925), (125, 513.924), (-125, 513.924)]))
-    center = Polygon(LineString([(0, -202.582), (-125, 13.925), (125, 13.925), (0, -202.582)]))
-    left = Polygon(LineString([(-125, 13.925), (-558.013, -236.075), (-433.013, -452.582), (0, -202.582), (-125, 13.925)]))
-    right = Polygon(LineString([(125, 13.925), (558.013, -236.075), (433.013, -452.582), (0, -202.582), (125, 13.925)]))
-
     ymaze_mouse = behavioral_df.copy()
-
-    # New column that gets the difference between row (n) and row (n-1)
+    time_spent_region = {}
     ymaze_mouse['time_diff'] = ymaze_mouse['#Snapshot Timestamp'].diff()
 
-    # Labels the different region in a dictionary
-    regions_dict = {'top': top, 'center': center, 'left': left, 'right': right}
-    time_spent_region = {}
+
+    # Labels the different regions in a dictionary
+    regions_coordinates = colin_sub_funcs.regions_dictionary()
 
     # Uses the key value pairs of regions_dict to label which region each coordinate resides
     # Lastly sums up the time spent in each region as a second dictionary
 
-    for key, value in regions_dict.items():
+    for key, value in regions_coordinates.items():
 
         ymaze_mouse[key] = ymaze_mouse.apply(lambda row: value.contains(Point(row["Position.X"], row["Position.Y"])),axis=1)
         time_spent_region[key] = ymaze_mouse[ymaze_mouse[key] == True]['time_diff'].sum()
 
     # Applies the region_match function to each row and gives a string label for where the coordinate was found
-    ymaze_mouse['region'] = ymaze_mouse.apply(region_match, axis=1)
+    ymaze_mouse['region'] = ymaze_mouse.apply(colin_sub_funcs.region_match, axis=1)
     first_time = ymaze_mouse['#Snapshot Timestamp'][0]
     first_region = ymaze_mouse['region'][0]
     time_spent_region[first_region] += first_time
@@ -90,15 +78,16 @@ def y_maze_time_spent(behavioral_df):
 
     out_df = ymaze_mouse.drop(columns=['top', 'center', 'left', 'right'])
 
+    # Set new key names to output dictionary
     new_names = {'center': 'center_time', 'left': 'left_time', 'right': 'right_time', 'top': 'top_time'}
-
-    # Final output dictionary with labels
     time_spent_region = dict((new_names[key], value) for (key, value) in time_spent_region.items())
 
-    # outputs dictionary with labels of time spent in each region.
-    # outputs df with 2 columns: time_diff, region
     return time_spent_region, out_df[['time_diff', 'region']]
 
+
+'''
+Organizes maze coordinates for function use. 
+'''
 
 def shapes(maze_array):
     if (maze_array.lower() == 'square') or (maze_array.lower() == 'of'):
---
+++ colin_sub_funcs.py
@@ -0,0 +1,71 @@
+from shapely.geometry import Point
+from shapely.geometry import Polygon, LineString, LinearRing
+import numpy as np
+
+# ----------- Functions for use in mouse_edge_distance() ------------ #
+
+'''
+Creates polygon of maze and inner maze for boundary separation. 
+
+Output 
+polygon_maze: Polygon of entire maze boundaries
+reduced_poly: Inner threshold polygon to detect mouse near wall
+
+'''
+
+def shape_shrink(maze_coordinates, distance_threshold):
+    if isinstance(maze_coordinates, int):
+        p = Point(0, 0)
+        inner_circle = maze_coordinates * (1 - distance_threshold)
+        polygon_maze = p.buffer(maze_coordinates)
+        reduced_poly = polygon_maze.difference(Point(0.0, 0.0).buffer(inner_circle))  # <- donut
+
+    else:
+        ident_mat = np.zeros((2, 2), float)
+        np.fill_diagonal(ident_mat, (1 - distance_threshold))
+        coords_t = maze_coordinates.transpose()
+        coords_red = np.matmul(ident_mat, coords_t)
+        polygon_maze = Polygon(maze_coordinates)
+        red_lin = LinearRing(coords_red.transpose())
+        reduced_poly = Polygon(maze_coordinates, [red_lin])
+
+    return polygon_maze, reduced_poly
+
+
+
+'''
+Checks that coordinates are in entire maze. 
+'''
+def within_bounds(df, polygon):
+    df['in_poly'] = df.apply(lambda row: polygon.contains(Point(row["Position.X"], row["Position.Y"])), axis=1)
+    if (~df['in_poly']).sum() != 0:
+        raise Exception('Warning! Mouse movement detected outside of Polygon boundaries.')
+
+
+# ----------- Functions for use in y_maze_time_spent() ------------ #
+
+def region_match(row):
+    if row['top']:
+        return 'top'
+    elif row['center']:
+        return 'center'
+    elif row['right']:
+        return 'right'
+    elif row['left']:
+        return 'left'
+    else:
+        raise Exception('Warning! Mouse movement detected outside of Polygon boundaries.')
+
+
+
+'''
+Organizes the coordinates for each ymaze region in a dictionary with region name as keys and coordinates as values
+'''
+
+def regions_dictionary():
+    top = Polygon(LineString([(-125, 513.924), (-125, 13.925), (125, 13.925), (125, 513.924), (-125, 513.924)]))
+    center = Polygon(LineString([(0, -202.582), (-125, 13.925), (125, 13.925), (0, -202.582)]))
+    left = Polygon(LineString([(-125, 13.925), (-558.013, -236.075), (-433.013, -452.582), (0, -202.582), (-125, 13.925)]))
+    right = Polygon(LineString([(125, 13.925), (558.013, -236.075), (433.013, -452.582), (0, -202.582), (125, 13.925)]))
+
+    return {'top': top, 'center': center, 'left': left, 'right': right}
