cc2b4bfd1f8bc34def2a73b6ecb3f12aaf0256a6
cmason30 <colinpetermason@gmail.com>
2021-06-30 14:02:28 -0600

    updated file structure

--- helper_functions/helper_functions1.py
+++ helper_functions/helper_functions1.py
@@ -2,6 +2,7 @@ import pandas as pd
 from shapely.geometry import Point
 from shapely.geometry import Polygon, LineString, LinearRing
 import numpy as np
+from helper_functions import helper_functions2
 import os
 
 # ------------ First line functions ----------------------- #
@@ -254,26 +255,125 @@ Gets total distance traveled for experiment
 
 """
 
-def total_distance_traveled(behavioral_df):
+
+def total_distance(behavioral_df):
     mouse_df = behavioral_df.copy()
     dx = (mouse_df['Position.X'] - mouse_df['Position.X'].shift())
     dy = (mouse_df['Position.Y'] - mouse_df['Position.Y'].shift())
     mouse_df['euclidean_dist'] = np.sqrt(dx ** 2 + dy ** 2)
-    total_distance = mouse_df['euclidean_dist'].sum()
-    return pd.DataFrame([total_distance], columns=['distance_traveled'])
+    final_value = mouse_df['euclidean_dist'].sum()
+    return pd.DataFrame([final_value], columns=['distance_traveled'])
 
-"""
-Creates rug applied column
 
-"""
+# ---------------------------------------- Master sheet generator ------------------------------------------------ #
+
+'''
+
+Organizes the functions to create a dataframe with all the significant variables as columns. 
+
+Input
+df_path: Give the file path of the behavioral file of interest.
+maze_array: Give one of four strings( 'square', 'circle', 'ymaze', 'corridor') based on the maze that the behavior file ran on
+master_path: Give the file path of the master sheet so that the new data being generated in this function appends to it. If generating
+a new master sheet, then just leave as None
+
+Output
+Outputs a dataframe with the comprehensive data from the statistical functions in the helper_functions files.
+Also outputs a second dataframe with descriptive stats gives region times and speed/velocity 
+'''
+
+def mouse_farm(df_path, maze_array, dist_threshold=.1):
+    mouse_df = pd.read_csv(df_path, header=2, sep='\t')
+    # file_path = pd.read_csv(df_path).iloc[0, 0]
+    file_name = df_path
+
+    mouse_df['filepath'] = df_path
+    mouse_df['maze_type'] = maze_array
+
+    mouse_distance = mouse_edge_distance(mouse_df, maze_array, dist_threshold)
+    mouse_df['speed'] = helper_functions2.calc_speed(mouse_df)
+
+    mouse_df['drug_applied'] = mouse_df.apply(lambda row: drug_applied(row['filepath']), axis=1)
+    # mouse_df['pairing_info'] = mouse_df.apply(lambda row: helper_functions1.pairing_info(row['filepath']), axis=1)
+
+    time_spent = y_maze_time_spent(mouse_df, file_name, maze_array)
+    total_distance_traveled = total_distance(mouse_df[['Position.X', 'Position.Y']])
+
+    final_df = pd.concat([mouse_df, mouse_distance, time_spent[1]], axis=1)
+    des_df_row = pd.concat([time_spent[0], helper_functions2.avg_velocity(mouse_df), total_distance_traveled], axis=1)
+    mesg = 'File Input.'
+    return final_df, des_df_row, mesg
+
+
+'''
+Helper function for mouse_farm(). Sends data to CSV. 
+
+Also checks that files are not duplicated in the master sheet. 
+
+mouse_dfpath: insert mouse_df here.
+maze_array: Include maze array o behavioral file. 
+master_path: Give it mastersheet file or path for new file. 
+
+output: Will make a new csv file or append it to an existing one. Also checks for duplicate experiment input. 
+'''
+
+
+def sheet1_appender(mouse_df, master_path, sep=',', copy=False):
+    if not os.path.isfile(master_path):
+        mouse_df.to_csv(master_path, mode='a', index=False, sep=sep)
+    else:
+        if len(mouse_df.columns) != len(pd.read_csv(master_path, nrows=1, sep=sep).columns):
+            raise Exception("Columns do not match!! Dataframe has " + str(len(mouse_df.columns)) + " columns. CSV file has " + str(len(pd.read_csv(master_path, nrows=1, sep=sep).columns)) + " columns.")
+        elif not (mouse_df.columns == pd.read_csv(master_path, nrows=1, sep=sep).columns).all():
+            raise Exception("Columns and column order of dataframe and csv file do not match!!")
+        else:
+            master = pd.read_csv(master_path)
+            if not copy:
+                if mouse_df['filepath'].unique()[0] in master['filepath'].unique():
+                    return print(f'Warning! Filepath[{mouse_df["filepath"].unique()[0]}] is already found in master sheet csv. Set copy=True to append anyway.')
+                else:
+                    mouse_df.to_csv(master_path, mode='a', index=False, sep=sep, header=False)
+
+            else:
+                mouse_df.to_csv(master_path, mode='a', index=False, sep=sep, header=False)
+
+
+# ---------Final Output Functions --------------- #
+
+
+
+'''
+Loops through all experiment files in a given directory and returns them as an organized CSV. 
+
+Input
+    directory: Given folder of behavioral files to read in.
+    master_path1: Desired mastersheet to input files to. Or input a new file name at end of file_path string and it will create a new masterhseet
+    maze_array: Give maze type
+    dist_threshold: For distance from wall function
+
+Output
+    1. Appends to or makes a new master sheet csv
+    2. Makes another csv of ymaze region time spent values. Or appends to an existing one.
+        This second csv will have the same name as the master sheet, but with *_sheet.csv at the end. 
+'''
+
+
+def experiment_output(directory, master_path1, maze_array, dist_threshold=.1):
+    for behavioral_file in file_walk(directory):
+        mouse_df = mouse_farm(behavioral_file, maze_array, dist_threshold)
+        sheet1_appender(mouse_df[0], master_path1)
+        desc_sheet2 = master_path1[:len(master_path1) - 4] + '_sheet2' + '.csv'
+        sheet1_appender(mouse_df[1], desc_sheet2)
+        print('File Input')
 
 
 def main():
     print('sup')
 
-if __name__ == "__main__":
-    main()
 
+if __name__ == "__main__":
+
+    main()
 
 
 
